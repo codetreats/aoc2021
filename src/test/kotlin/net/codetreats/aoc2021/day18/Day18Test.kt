6c920e253f5d0a1dccef2e01f54f9355a909c684
Martin <github@msturm.net>
2021-12-03 20:57:50 +0100

    Add skeletons for all days

---
+++ src/test/kotlin/net/codetreats/aoc2021/day18/Day18Test.kt
@@ -0,0 +1,38 @@
+package net.codetreats.aoc2021.day18
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class Day18Test {
+    var classUnderTest = Day18()
+
+    @BeforeEach
+    fun setup() {
+
+    }
+
+    @Test
+    fun testPart01() {
+        classUnderTest.init(false)
+        assertEquals("", classUnderTest.run1())
+    }
+
+    @Test
+    fun testPart01_dummyData() {
+        classUnderTest.init(true)
+        assertEquals("", classUnderTest.run1())
+    }
+
+    @Test
+    fun testPart02() {
+        classUnderTest.init(false)
+        assertEquals("", classUnderTest.run2())
+    }
+
+    @Test
+    fun testPart02_dummyData() {
+        classUnderTest.init(true)
+        assertEquals("", classUnderTest.run2())
+    }
+}
