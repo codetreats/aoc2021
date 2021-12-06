81f2f77fb69a5692eca833d8ad421f775e453a7a
Martin <github@msturm.net>
2021-12-06 10:02:45 +0100

    Add day 6

--- src/main/kotlin/net/codetreats/aoc2021/day06/Day06.kt
+++ src/main/kotlin/net/codetreats/aoc2021/day06/Day06.kt
@@ -3,20 +3,45 @@ package net.codetreats.aoc2021.day06
 import net.codetreats.aoc2021.Day
 import net.codetreats.aoc2021.util.Logger
 
-class Day06 : Day<List<String>>(6) {
+class Day06 : Day<List<Int>>(6) {
     override val logger: Logger = Logger.forDay(dayOfMonth)
 
-    override fun useDummy(): Boolean = true
+    override fun useDummy(): Boolean = false
 
-    override fun default(): List<String> = listOf()
+    override fun default(): List<Int> = listOf()
 
-    override fun convert(input: List<String>): List<String> = input
+    override fun convert(input: List<String>): List<Int> = input[0].split(",").map { it.toInt() }
 
-    override fun run1(): String {
-        return ""
+    override fun run1(): String = simulate(80).toString()
+
+    override fun run2(): String = simulate(256).toString()
+
+    private fun simulate(days: Int) : Long {
+        var fishes = inputSortedBySlots()
+        for (day in 1..days) {
+            val birthing = fishes[0]
+            for (i in 0 .. 7) {
+                // move fishes to next lower slot
+                fishes[i] = fishes[i + 1]
+            }
+            fishes[6] = fishes[6] + birthing // reset fish counter to 6 for all fishes that created new fishes
+            fishes[8] = birthing // create new fishes for each birthing fish
+        }
+        return fishes.sum()
     }
 
-    override fun run2(): String {
-        return ""
+    /**
+     * Sort the fishes into slots that indicates, how many days they have left until the create a new fish
+     * Therefore there are exactly 9 slots (0 - 8)
+     */
+    private fun inputSortedBySlots() : MutableList<Long> {
+        val fishes = mutableListOf<Long>()
+        for (i in 0 .. 8) {
+            fishes.add(0L)
+        }
+        input.forEach {
+            fishes[it] = fishes[it] + 1
+        }
+        return fishes
     }
 }
--- src/test/kotlin/net/codetreats/aoc2021/day06/Day06Test.kt
+++ src/test/kotlin/net/codetreats/aoc2021/day06/Day06Test.kt
@@ -15,24 +15,24 @@ class Day06Test {
     @Test
     fun testPart01() {
         classUnderTest.init(false)
-        assertEquals("", classUnderTest.run1())
+        assertEquals("389726", classUnderTest.run1())
     }
 
     @Test
     fun testPart01_dummyData() {
         classUnderTest.init(true)
-        assertEquals("", classUnderTest.run1())
+        assertEquals("5934", classUnderTest.run1())
     }
 
     @Test
     fun testPart02() {
         classUnderTest.init(false)
-        assertEquals("", classUnderTest.run2())
+        assertEquals("1743335992042", classUnderTest.run2())
     }
 
     @Test
     fun testPart02_dummyData() {
         classUnderTest.init(true)
-        assertEquals("", classUnderTest.run2())
+        assertEquals("26984457539", classUnderTest.run2())
     }
 }
