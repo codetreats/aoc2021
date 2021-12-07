9a6e592e8fab11e5ccd2bff0d13aaa9afa25be53
Martin <github@msturm.net>
2021-12-07 20:02:48 +0100

    Add day 7

--- src/main/kotlin/net/codetreats/aoc2021/day07/Day07.kt
+++ src/main/kotlin/net/codetreats/aoc2021/day07/Day07.kt
@@ -2,21 +2,26 @@ package net.codetreats.aoc2021.day07
 
 import net.codetreats.aoc2021.Day
 import net.codetreats.aoc2021.util.Logger
+import kotlin.math.abs
 
-class Day07 : Day<List<String>>(7) {
+class Day07 : Day<List<Int>>(7) {
     override val logger: Logger = Logger.forDay(dayOfMonth)
 
-    override fun useDummy(): Boolean = true
+    override fun useDummy(): Boolean = false
 
-    override fun default(): List<String> = listOf()
+    override fun default(): List<Int> = listOf()
 
-    override fun convert(input: List<String>): List<String> = input
+    override fun convert(input: List<String>): List<Int> = input[0].split(",").map { it.toInt() }
 
-    override fun run1(): String {
-        return ""
-    }
+    override fun run1(): String = calculateFuel(false).toString()
 
-    override fun run2(): String {
-        return ""
-    }
+    override fun run2(): String = calculateFuel(true).toString()
+
+    private fun calculateFuel(sumUp: Boolean) =
+        IntRange(input.min()!!,input.max()!!).map { calculateFuel(it, sumUp) }.min()
+
+    private fun calculateFuel(pos: Int, sumUp: Boolean) =
+        input.map { abs(pos - it) }.map { if (sumUp) sumUp(it) else it }.sum()
+
+    private fun sumUp(until : Int) = until * (until + 1) / 2
 }
--- src/test/kotlin/net/codetreats/aoc2021/day07/Day07Test.kt
+++ src/test/kotlin/net/codetreats/aoc2021/day07/Day07Test.kt
@@ -15,24 +15,24 @@ class Day07Test {
     @Test
     fun testPart01() {
         classUnderTest.init(false)
-        assertEquals("", classUnderTest.run1())
+        assertEquals("347011", classUnderTest.run1())
     }
 
     @Test
     fun testPart01_dummyData() {
         classUnderTest.init(true)
-        assertEquals("", classUnderTest.run1())
+        assertEquals("37", classUnderTest.run1())
     }
 
     @Test
     fun testPart02() {
         classUnderTest.init(false)
-        assertEquals("", classUnderTest.run2())
+        assertEquals("98363777", classUnderTest.run2())
     }
 
     @Test
     fun testPart02_dummyData() {
         classUnderTest.init(true)
-        assertEquals("", classUnderTest.run2())
+        assertEquals("168", classUnderTest.run2())
     }
 }
