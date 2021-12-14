2c0f3098977d8cfed70125c5fc0cef04f411d59d
Martin <github@msturm.net>
2021-12-14 10:39:49 +0100

    Add day 14

--- src/main/kotlin/net/codetreats/aoc2021/day14/Day14.kt
+++ src/main/kotlin/net/codetreats/aoc2021/day14/Day14.kt
@@ -2,21 +2,72 @@ package net.codetreats.aoc2021.day14
 
 import net.codetreats.aoc2021.Day
 import net.codetreats.aoc2021.util.Logger
+import kotlin.text.StringBuilder
 
-class Day14 : Day<List<String>>(14) {
+class Day14 : Day<Map<String, List<String>>>(14) {
+    private var template = ""
+
     override val logger: Logger = Logger.forDay(dayOfMonth)
 
     override fun useDummy(): Boolean = true
 
-    override fun default(): List<String> = listOf()
+    override fun default(): Map<String, List<String>> = mapOf()
 
-    override fun convert(input: List<String>): List<String> = input
-
-    override fun run1(): String {
-        return ""
+    override fun convert(input: List<String>): Map<String, List<String>> {
+        template = input[0]
+        var result = mutableMapOf<String, List<String>>()
+        for (i in 2 until input.size) {
+            val (from, to) = input[i].split(" -> ")
+            result[from] = listOf("${from[0]}${to[0]}", "${to[0]}${from[1]}")
+        }
+        return result
     }
 
-    override fun run2(): String {
-        return ""
+    override fun run1(): String = runSteps(10).toString()
+
+    override fun run2(): String = runSteps(40).toString()
+
+    private fun runSteps(amount: Int): Long {
+        var chunks = templateInChunks()
+        for (step in 1..amount) {
+            var nextChunks = mutableMapOf<String, Long>()
+            chunks.forEach { (key, amount) ->
+                input[key]!!.forEach { nextChunks.increase(it, amount) }
+            }
+            chunks = nextChunks
+            logger.info("After step $step: $chunks")
+        }
+        return calculateResult(chunks)
+    }
+
+    private fun templateInChunks(): Map<String, Long> {
+        var chunks = mutableMapOf<String, Long>()
+        for (i in 1 until template.length) {
+            val key = "${template[i - 1]}${template[i]}"
+            chunks[key] = 1 + (chunks[key] ?: 0)
+        }
+        logger.info("Input ($template) as Chunks: $chunks")
+        return chunks
+    }
+
+    private fun calculateResult(chunks: Map<String, Long>): Long {
+        val map = mutableMapOf<Char, Long>()
+
+        chunks.forEach {
+            map.increase(it.key[0], it.value)
+            map.increase(it.key[1], it.value)
+        }
+
+        // all but the first and the last char are counted twice, as the chunks are overlapping
+        map.increase(template.first())
+        map.increase(template.last())
+        val max = map.values.max()!! / 2
+        val min = map.values.min()!! / 2
+
+        return max - min
+    }
+
+    private fun <T> MutableMap<T, Long>.increase(pos: T, add: Long = 1) {
+        this[pos] = getOrDefault(pos, 0) + add
     }
 }
--- src/test/kotlin/net/codetreats/aoc2021/day14/Day14Test.kt
+++ src/test/kotlin/net/codetreats/aoc2021/day14/Day14Test.kt
@@ -15,24 +15,24 @@ class Day14Test {
     @Test
     fun testPart01() {
         classUnderTest.init(false)
-        assertEquals("", classUnderTest.run1())
+        assertEquals("2891", classUnderTest.run1())
     }
 
     @Test
     fun testPart01_dummyData() {
         classUnderTest.init(true)
-        assertEquals("", classUnderTest.run1())
+        assertEquals("1588", classUnderTest.run1())
     }
 
     @Test
     fun testPart02() {
         classUnderTest.init(false)
-        assertEquals("", classUnderTest.run2())
+        assertEquals("4607749009683", classUnderTest.run2())
     }
 
     @Test
     fun testPart02_dummyData() {
         classUnderTest.init(true)
-        assertEquals("", classUnderTest.run2())
+        assertEquals("2188189693529", classUnderTest.run2())
     }
 }
