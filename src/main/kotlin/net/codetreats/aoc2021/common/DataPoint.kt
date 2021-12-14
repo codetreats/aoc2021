f2a6618792150f23c7d39b183a034b6aae296ca1
Martin <github@msturm.net>
2021-12-14 10:37:38 +0100

    Fix common

--- src/main/kotlin/net/codetreats/aoc2021/common/DataPoint.kt
+++
@@ -1,5 +0,0 @@
-package net.codetreats.aoc2021.common;
-
-data class DataPoint<T>(override val x: Int, override val y: Int, val value: T) : Point
-
-typealias IntPoint = DataPoint<Int>
--- src/main/kotlin/net/codetreats/aoc2021/common/Point.kt
+++ src/main/kotlin/net/codetreats/aoc2021/common/Point.kt
@@ -3,4 +3,23 @@ package net.codetreats.aoc2021.common
 interface Point {
     val x: Int
     val y: Int
+
+    companion object {
+        fun from(x: Int, y: Int): Point = SinglePoint(x,y)
+    }
 }
+
+data class SinglePoint(override val x: Int, override val y: Int) : Point, Comparable<Point> {
+    override fun toString(): String = "($x,$y)"
+
+    override fun compareTo(other: Point): Int {
+        if (x == other.x) {
+            return y.compareTo(other.y)
+        }
+        return x.compareTo(other.x)
+    }
+}
+
+data class DataPoint<T>(override val x: Int, override val y: Int, val value: T) : Point
+
+typealias IntPoint = DataPoint<Int>
